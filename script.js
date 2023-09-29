8475e1380055794aefceb681ad41d3d3382eede8
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-29 10:53:30 -0300

    Atualizando Arquivos

---
+++ script.js
@@ -0,0 +1,37 @@
+// 5 - No arquivo script.js, criar uma lista de variáveis com os seguintes nomes:
+// - nome que recebe o seu nome
+// - idade que recebe a sua idade
+// - status com um valor booleano verdadeiro
+// - valorProduto que recebe um valor número flutuante, com o preço de 10.50
+// - listaTimes com uma Array com três times.
+let nome = "Lucas"
+let idade = 24
+let status = true
+let valorProduto = 10.50
+let Times = ["Barcelona", "Milan", "Portuguesa"]
+
+
+// 6 - Listar cada variável em um console.log
+console.log(nome)
+console.log(idade)
+console.log(status)
+console.log(valorProduto)
+console.log(Times)
+
+
+// 7 - No mesmo arquivo, criar operações matemáticas com 4 variáveis:
+// - soma
+// - subtracao
+// - multiplicacao
+// - divisao
+let soma = 300 + 5
+let subtracao = 300 - 5
+let multiplicacao = 300 * 5
+let divisao = 300 / 5
+
+
+// 8 - Mostrar os resultados no console.log
+console.log(soma)
+console.log(subtracao)
+console.log(multiplicacao)
+console.log(divisao)
